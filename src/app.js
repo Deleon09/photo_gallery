a69e5b887be699544a965a5ebacfa087c9be540f
Alberto <alberto101481@gmail.com>
2023-08-31 22:36:54 -0600

    :tada: ADD: Project start

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
+const exphbs = require('express-handlebars');
+ 
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'), // __dirname + '/views/layouts'
+    partialsDir: path.join(app.get('views'), 'partials'), // __dirname + '/views/partials'
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); //para entender los datos que envia el formulario
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'), //directorio donde se guardan las imagenes
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + path.extname(file.originalname)); //nombre de la imagen
+    }
+});
+app.use(multer({ storage }).single('image'));
+
+// Routes
+app.use(require('./routes'));
+
+module.exports = app;
